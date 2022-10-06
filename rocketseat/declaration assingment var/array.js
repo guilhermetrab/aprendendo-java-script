db029c813ea71cc990fe2c6f037023eaadef1562
Guilherme <108558971+guilhermetrab@users.noreply.github.com>
2022-10-05 22:39:30 -0300

    .

---
+++ rocketseat/declaration assingment var/array.js
@@ -0,0 +1,10 @@
+const animals =[
+    'Lion',
+    'Monkey',
+    {
+        nome: 'Cat',
+        age:3     
+    }
+]
+console.log(animals.length)
+console.log(animals[2].nome)
---
+++ rocketseat/declaration assingment var/obeject.js
@@ -0,0 +1,7 @@
+const person = {
+    nome: 'John',
+    age: 30,
+    weigth: 88.6,
+    isAdmin: true
+}
+console.log(`${person.nome} tem ${person.age} `)
